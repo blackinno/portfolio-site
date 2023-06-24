92000e068ef72ff43b0d22a9398f085e09901856
Thotsaphon Ruthamnong <ruthotsaphon@centralpattana.co.th>
2023-06-24 13:56:26 +0700

    first init

---
+++ src/components/headers/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Headers: React.FC = props => {
+  return (
+    <Head>
+      <title>Website's Portfolio</title>
+    </Head>
+  )
+}
+
+export default Headers
